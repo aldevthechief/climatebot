78c2523e7afd703961d5d822d886a376005a2ac6
aldev <67814691+aldevthechief@users.noreply.github.com>
2024-05-08 21:48:27 +1000

    Update main.py

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ def run_bot():
 
     @bot.message_handler(commands=['weather'])
     def weather(message):
-        print(schedule.get_jobs())
+        print(schedule.get_jobs(), flush=True)
         chatid = message.chat.id
         bot.clear_step_handler_by_chat_id(chatid)
         
